6ceda46cdb2202bc264ec1adb2659133b55a00fc
Robert Quintana <robertquintanatx@gmail.com>
2020-06-04 10:54:55 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,71 @@
-def new_hash
+require "pry"
 
+def new_hash
+return {}
 end
 
 def actor
-
+actor = {
+  :name => "Dwayne The Rock Johnson"
+}
 end
 
 def monopoly
 	monopoly = {}
-
+  monopoly[:railroads] = {}
+  monopoly
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+  monopoly = {}
+  monopoly[:railroads] = {}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+  monopoly = {}
+  monopoly[:railroads] = {}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
+  rent_num = monopoly[:railroads][:rent_in_dollars]
+  rail_names = monopoly[:railroads][:names]
+  rent_num[:one_piece_owned] = 25
+  rent_num[:two_pieces_owned] = 50
+  rent_num[:three_pieces_owned] = 100
+  rent_num[:four_pieces_owned] = 200
+  rail_names[:reading_railroad] = {}
+  rail_names[:pennsylvania_railroad] = {}
+  rail_names[:b_and_o_railroad] = {}
+  rail_names[:shortline] = {}
+  monopoly
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+  monopoly = {}
+  monopoly[:railroads] = {}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:names] = {}
+  rent_num = monopoly[:railroads][:rent_in_dollars]
+  rail_names = monopoly[:railroads][:names]
+  rent_num[:one_piece_owned] = 25
+  rent_num[:two_pieces_owned] = 50
+  rent_num[:three_pieces_owned] = 100
+  rent_num[:four_pieces_owned] = 200
+  rail_names[:reading_railroad] = {}
+  rail_names[:pennsylvania_railroad] = {}
+  rail_names[:b_and_o_railroad] = {}
+  rail_names[:shortline] = {}
+  rail_names[:reading_railroad]["mortgage_value"] = '$100'
+  rail_names[:pennsylvania_railroad]["mortgage_value"] = '$200'
+  rail_names[:b_and_o_railroad]["mortgage_value"] = "$400"
+  rail_names[:shortline]["mortgage_value"] = '$800'
+  monopoly
 end
+
+monopoly_with_fourth_tier
+
